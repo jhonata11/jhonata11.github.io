d257c07c997d37c00f6f304927a9adfece156056
jhonata11 <jhonata11@hotmail.com>
2020-10-07 22:24:02 +0000

    Deploying to gh-pages from  @ 4b07d8cc559120e96858475c50ec842cd5d276d0 🚀

---
+++ precache-manifest.bfd6c2aa4abaf6a1e69a58efa89792aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e4be356f39ec9bb1a1d52f2ae187f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba225bc2808e9228bd76",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85ee1be2406d0b5e4f09",
+    "url": "/static/js/2.94ac1ac9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94ac1ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba225bc2808e9228bd76",
+    "url": "/static/js/main.87f20529.chunk.js"
+  },
+  {
+    "revision": "2f8ee401bf304be4d768",
+    "url": "/static/js/runtime-main.0d8aae82.js"
+  }
+]);
