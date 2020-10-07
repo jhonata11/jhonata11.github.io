287bda4affd743f64dc8901767dc2e5d1d365995
jhonata11 <jhonata11@hotmail.com>
2020-10-07 23:12:25 +0000

    Deploying to master from  @ e4bcf77ff76c604737ff7e14c1e8eb0692504717 🚀

---
+++ precache-manifest.252fbf2cea5242056e17e6ea81c92b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a013677471807b891eb6b085325d177b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "887142a37443e3bd8460",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "85ee1be2406d0b5e4f09",
+    "url": "/static/js/2.94ac1ac9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94ac1ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887142a37443e3bd8460",
+    "url": "/static/js/main.5cfdd3a9.chunk.js"
+  },
+  {
+    "revision": "2f8ee401bf304be4d768",
+    "url": "/static/js/runtime-main.0d8aae82.js"
+  }
+]);
